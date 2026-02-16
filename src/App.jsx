5bf066aa3b623928a649c3d75f3ff8ed6b716ae1
Abhish <code.abhish@gmail.com>
2026-02-16 17:41:58 +0530

    fix the file related bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 import Navbar from './component/Navbar.jsx'
 import Manager from './component/Manager.jsx'
 
